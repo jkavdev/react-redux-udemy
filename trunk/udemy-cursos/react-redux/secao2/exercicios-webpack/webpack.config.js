88320967e8302da7828c8edb871d66064fd80a87
jhonatankolen <jhonatankolen@b532a151-d5e8-4e7d-bcad-1a85d0c4e1cd>
2017-06-09 21:13:32 +0000

    Seção 2 - Webpack - 8 - Testando a configuração
    - Executando console.loe, testando o webpack

--- trunk/udemy-cursos/react-redux/secao2/exercicios-webpack/webpack.config.js
+++ trunk/udemy-cursos/react-redux/secao2/exercicios-webpack/webpack.config.js
@@ -1,16 +1,16 @@
 //realizando referencia ao dos modulos webpack
 const webpack = require('webpack')
 
-//Cofiguração do webpack, tornando visivel o que configuramos    
+//Cofiguracao do webpack, tornando visivel o que configuramos    
 module.exports = {
-    //Conterá as definições para o webpack
+    //Contera as definicoes para o webpack
     entry: './ex/index.js',
-    //Configuração de saida do webpack, arquivo que será gerado 
+    //Configuracao de saida do webpack, arquivo que sera gerado 
     output: {
         path: __dirname + '/public', 
         filename: './bundle.js'
     },
-    //Configuração do servidor
+    //Configuracao do servidor
     devServer: {
         port: 8080,
         contentBase: './public'
