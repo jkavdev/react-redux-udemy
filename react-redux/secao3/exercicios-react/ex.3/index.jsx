32c68ed8d8d1fd2a841dcfeb50948d1fcf73cce8
jhonatankolen <jhonatankolen@b532a151-d5e8-4e7d-bcad-1a85d0c4e1cd>
2017-06-13 21:49:49 +0000

    Seção 3 - React - 21 - Primeiro Componente (Arrow Function)
    - Utilizando Arrow Function

---
+++ react-redux/secao3/exercicios-react/ex.3/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+//Importando um componente do react sem informar a extensao do arquivo
+import Component from './component'
+
+
+ReactDOM.render(<Component />, document.getElementById('app'))
