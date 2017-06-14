d9f69dc60ae0770ed3d28be26da66ff78dbd8486
jhonatankolen <jhonatankolen@b532a151-d5e8-4e7d-bcad-1a85d0c4e1cd>
2017-06-14 22:34:08 +0000

    Seção 4 - TodoApp (Backend) - 36 - Mapeamento das Rotas

--- trunk/udemy-cursos/react-redux/secao4/todo-app/backend/src/loader.js
+++ trunk/udemy-cursos/react-redux/secao4/todo-app/backend/src/loader.js
@@ -1,3 +1,5 @@
 //arquivos de configuracao
-require('./config/server')
+const server = require('./config/server')
 require('./config/database')
+//passando o servidor para o arquivo de configuracao das rotas
+require('./config/routes')(server)
