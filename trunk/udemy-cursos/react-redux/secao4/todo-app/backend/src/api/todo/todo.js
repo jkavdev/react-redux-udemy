0254a71cec998268166547344c002fd3ffcf882c
jhonatankolen <jhonatankolen@b532a151-d5e8-4e7d-bcad-1a85d0c4e1cd>
2017-06-14 22:13:35 +0000

    Seção 4 - TodoApp (Backend) - 35 - ODM e Criação da API REST
    - Criação do schema do todo e criação da api rest

---
+++ trunk/udemy-cursos/react-redux/secao4/todo-app/backend/src/api/todo/todo.js
@@ -0,0 +1,13 @@
+const restful = require('./node-restful')
+//api rest para trabalhar com o mongoose
+const mongoose = restful.mongoose
+
+//Criando o schema a ser usado no mongodb
+const todoSchema = new mongoose.Schema({
+    description: {type: String, require: true},
+    description: {type: Boolean, require: true, default: false},
+    createdAt: {type: Date, default: Date.now}
+})
+
+//Exportando o schema
+module.exports = restful.model('Todo', todoSchema)
