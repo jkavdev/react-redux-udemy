2b76b3ec0e72612fffc8af9977adcb2b540ea56c
jhonatankolen <jhonatankolen@b532a151-d5e8-4e7d-bcad-1a85d0c4e1cd>
2017-06-15 18:31:31 +0000

    Criando seção 5

---
+++ react-redux/secao5/todo-app/backend/src/config/server.js
@@ -0,0 +1,24 @@
+//Porta do servidor
+const port = 3003
+
+//midleware para o parse da requisicao
+const bodyParser = require('body-parser')
+//servidor de aplicacao
+const express = require('express')
+//criando instancia do servidor
+const server = express()
+//permitindo o cors
+const allowCors = require('./cors')
+
+//aplicando os midlewares para cada requisicao realizado
+server.use(bodyParser.urlencoded({ extended: true }))
+server.use(bodyParser.json())
+server.use(allowCors)
+
+//atribuindo a porta ao servido e apresentando mensagem de sucesso
+server.listen(port, function () {
+    console.log(`BACKEND is running on port ${port}`)
+})
+
+//exportando o servidor pois podemos usa-lo para registro das rotas
+module.exports = server
