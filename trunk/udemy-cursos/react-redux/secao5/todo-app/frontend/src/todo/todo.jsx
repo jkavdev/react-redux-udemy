26360e571875aa50340ae853293446040010c902
jhonatankolen <jhonatankolen@b532a151-d5e8-4e7d-bcad-1a85d0c4e1cd>
2017-06-19 17:29:42 +0000

    Seção 5 - TodoApp (Frontend) - 58 - Pesquisa de TODOs
    - Adicionando busca de tarefas com botão para pesquisa de tarefas

--- trunk/udemy-cursos/react-redux/secao5/todo-app/frontend/src/todo/todo.jsx
+++ trunk/udemy-cursos/react-redux/secao5/todo-app/frontend/src/todo/todo.jsx
@@ -20,14 +20,27 @@ export default class Todo extends Component {
         this.handleRemove = this.handleRemove.bind(this)
         this.handleMarkAsDone = this.handleMarkAsDone.bind(this)
         this.handleMarkAsPending = this.handleMarkAsPending.bind(this)
+        this.handleSearch = this.handleSearch.bind(this)
 
         this.refresh()
     }
 
     //atualiza o estado do componente lista e descricacao, utilizado no inicio do render, e como callback de outras funcoes
-    refresh() {
-        axios.get(`${URL}?sort=-createdAt`)
-            .then(resp => this.setState({ ...this.state, description: '', list: resp.data }))
+
+    //fara a busca de acordo com um descricao, se nao passarmos uma nao nao adicionamos ela a codicao da busca
+
+    //quando usarmos refresh precimas indicar a descricao de cada contexto de cada acao que devemos realizar
+    //tipo quando adicionar, nao precisa informar uma descricao
+    //mas quando for exluir, ou alterar indicamos o valor como o estado atual do componente de busca
+    refresh(description = '') {
+        const search = description ? `&description__regex=/${description}/` : ''
+        axios.get(`${URL}?sort=-createdAt${search}`)
+            .then(resp => this.setState({ ...this.state, description, list: resp.data }))
+    }
+
+    //realiza a busca do bota de acordo com a descricao do campo
+    handleSearch() {
+        this.refresh(this.state.description)
     }
 
     //Alterando o estado do componente de acordo com os valores atualizados passados pelo evento
@@ -47,19 +60,19 @@ export default class Todo extends Component {
     //removendo as tarefas passando o todo com o id
     handleRemove(todo) {
         axios.delete(`${URL}/${todo._id}`)
-            .then(resp => this.refresh())
+            .then(resp => this.refresh(this.state.description))
     }
 
     //altera a tarefa para finalizada
     handleMarkAsDone(todo) {
         axios.put(`${URL}/${todo._id}`, { ...todo, done: true })
-            .then(resp => this.refresh())
+            .then(resp => this.refresh(this.state.description))
     }
 
     //altera a tarefa para nao finalizada
     handleMarkAsPending(todo) {
         axios.put(`${URL}/${todo._id}`, { ...todo, done: false })
-            .then(resp => this.refresh())
+            .then(resp => this.refresh(this.state.description))
     }
 
     render() {
@@ -70,7 +83,8 @@ export default class Todo extends Component {
                 <TodoForm 
                     description={this.state.description}
                     handleAdd={this.handleAdd}
-                    handleChange={this.handleChange} />
+                    handleChange={this.handleChange} 
+                    handleSearch={this.handleSearch}/>
                 <TodoList 
                     list={this.state.list}
                     handleMarkAsDone={this.handleMarkAsDone}
