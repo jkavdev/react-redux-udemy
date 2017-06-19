6d543c2f7994c471c0788e3ccf5b55adfbbb0746
jhonatankolen <jhonatankolen@b532a151-d5e8-4e7d-bcad-1a85d0c4e1cd>
2017-06-19 01:27:24 +0000

    Seção 5 - TodoApp (Frontend) - 47 - Componente PageHeader
    - Adicionando um componente de cabeçalho

--- trunk/udemy-cursos/react-redux/secao5/todo-app/frontend/src/main/app.jsx
+++ trunk/udemy-cursos/react-redux/secao5/todo-app/frontend/src/main/app.jsx
@@ -2,7 +2,7 @@ import 'modules/bootstrap/dist/css/bootstrap.min.css'
 import 'modules/font-awesome/css/font-awesome.min.css'
 
 import React from 'react'
-import Menu from '../menu/menu'
+import Menu from '../template/menu'
 import Routes from './routes'
 
 export default props => (
