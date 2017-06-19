a44ecedf1633441ea08637b6812ecd664c0b5c21
jhonatankolen <jhonatankolen@b532a151-d5e8-4e7d-bcad-1a85d0c4e1cd>
2017-06-19 03:41:39 +0000

    Seção 5 - TodoApp (Frontend) - 52 - Renderização Condicional (If)
    - Adicionando componente para condicação do react

--- trunk/udemy-cursos/react-redux/secao5/todo-app/frontend/src/template/iconButton.jsx
+++ trunk/udemy-cursos/react-redux/secao5/todo-app/frontend/src/template/iconButton.jsx
@@ -1,15 +1,13 @@
 import React from 'react'
+import If from './if'
 
 //Componentizando o botao, informando seu estilo, boostrap e o tipo do icone font-awesome
-export default props => {
-    if (props.hide) {
-        return null
-    } else {
-        return (
-            <button className={'btn btn-' + props.style}
-                onClick={onclick}>
-                <i className={'fa fa-' + props.icon}></i>
-            </button >
-        )
-    }
-}
+export default props => (
+    //Utilizando o componente if, se hide=true, nao mostra botao e vice-versa
+    <If test={!props.hide}>
+        <button className={'btn btn-' + props.style}
+            onClick={onclick}>
+            <i className={'fa fa-' + props.icon}></i>
+        </button >
+    </If>
+)
