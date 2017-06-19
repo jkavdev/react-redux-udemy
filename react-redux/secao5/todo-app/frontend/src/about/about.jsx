3ff173ebd384f011d52d9d43e58412481b9a6616
jhonatankolen <jhonatankolen@b532a151-d5e8-4e7d-bcad-1a85d0c4e1cd>
2017-06-19 01:27:24 +0000

    Seção 5 - TodoApp (Frontend) - 47 - Componente PageHeader
    - Adicionando um componente de cabeçalho

--- react-redux/secao5/todo-app/frontend/src/about/about.jsx
+++ react-redux/secao5/todo-app/frontend/src/about/about.jsx
@@ -1,7 +1,15 @@
 import React from 'react'
+import PageHeader from '../template/pageHeader'
 
 export default props => (
     <div>
-        <h1>Sobre</h1>
+        <PageHeader name='Sobre' small='Nós'></PageHeader>
+
+        <h2>Nossa História</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Expedita ducimus reiciendis accusantium adipisci, nobis! Quos debitis tempora repudiandae doloremque autem repellendus neque soluta nisi! Ad repellat obcaecati dolorem sint est!</p>
+        <h2>Missão e Valor</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Expedita ducimus reiciendis accusantium adipisci, nobis! Quos debitis tempora repudiandae doloremque autem repellendus neque soluta nisi! Ad repellat obcaecati dolorem sint est!</p>
+        <h2>Imprensa</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Expedita ducimus reiciendis accusantium adipisci, nobis! Quos debitis tempora repudiandae doloremque autem repellendus neque soluta nisi! Ad repellat obcaecati dolorem sint est!</p>
     </div>
 )
