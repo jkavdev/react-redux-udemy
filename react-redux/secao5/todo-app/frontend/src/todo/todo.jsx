58ab2c80a3deba539fd2c99c66f2c407dda72a82
jhonatankolen <jhonatankolen@b532a151-d5e8-4e7d-bcad-1a85d0c4e1cd>
2017-06-19 03:57:48 +0000

    Seção 5 - TodoApp (Frontend) - 53 - Evento Adicionar
    - Adicionando evento do botão adicionar tarefa

--- react-redux/secao5/todo-app/frontend/src/todo/todo.jsx
+++ react-redux/secao5/todo-app/frontend/src/todo/todo.jsx
@@ -5,12 +5,25 @@ import TodoForm from './todoForm'
 
 export default class Todo extends Component {
 
+    constructor(props) {
+        super(props)
+
+        //precisamos ligar o contexto da funcao com o contexto do componente
+        this.handleAdd = this.handleAdd.bind(this)
+    }
+
+    handleAdd() {
+        console.log('Adicionando.....')
+        //tem que ser o contexto do componente
+        console.log(this)
+    }
+
     render() {
         return (
             <div>
                 <PageHeader name='Tarefas' small='Cadastro'></PageHeader>
 
-                <TodoForm />
+                <TodoForm handleAdd={this.handleAdd} />
                 <TodoList />
             </div>
         )
