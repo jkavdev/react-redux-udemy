a07201324bae9742fa03809906d8f0fc3054d789
jhonatankolen <jhonatankolen@b532a151-d5e8-4e7d-bcad-1a85d0c4e1cd>
2017-06-19 01:40:56 +0000

    Seção 5 - TodoApp (Frontend) - 48 - Componentes TodoForm e TodoList
    - Adicionando componentes para o formulário e listagem das tarefas

--- trunk/udemy-cursos/react-redux/secao5/todo-app/frontend/src/todo/todo.jsx
+++ trunk/udemy-cursos/react-redux/secao5/todo-app/frontend/src/todo/todo.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
 import PageHeader from '../template/pageHeader'
+import TodoList from './todoList'
+import TodoForm from './todoForm'
 
 export default class Todo extends Component {
 
@@ -7,6 +9,9 @@ export default class Todo extends Component {
         return (
             <div>
                 <PageHeader name='Tarefas' small='Cadastro'></PageHeader>
+
+                <TodoForm />
+                <TodoList />
             </div>
         )
     }
