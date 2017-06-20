cdd7b44c4298bcf342a5f5b12cff267c31e41a87
jhonatankolen <jhonatankolen@b532a151-d5e8-4e7d-bcad-1a85d0c4e1cd>
2017-06-20 16:26:31 +0000

    Seção 6 - React com Redux - 63 - Configurando o Redux
     - Executando componente react, configurado o redux

--- trunk/udemy-cursos/react-redux/secao6/exercicios-react-redux/ex/index.jsx
+++ trunk/udemy-cursos/react-redux/secao6/exercicios-react-redux/ex/index.jsx
@@ -2,4 +2,22 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import Field from './field'
 
-ReactDOM.render(<Field initialValue='Testai' />, document.getElementById('app'))
+//createStore - cria o estado unico, o estado pode ter varios atributos, varios estados da aplicacao
+//combineReducers - liga os reducers para cada atributo criado na store, gerando um objeto unico com os estados
+import { combineReducers, createStore } from 'redux'
+//Componente que liga os estados criados com o redux para os componentes do react
+import { Provider } from 'react-redux'
+
+//ligando os reducers com os campos
+//um campo sempre eh ligado a um reducer
+//reducer - uma funcao
+const reducers = combineReducers({
+    field: () => ({ value: 'Opa' })
+})
+
+ReactDOM.render(
+    //criando provide fornecendo a store com os reducers
+    <Provider store={createStore(reducers)}>
+        <Field initialValue='Testai' />
+    </Provider>,
+    document.getElementById('app'))
