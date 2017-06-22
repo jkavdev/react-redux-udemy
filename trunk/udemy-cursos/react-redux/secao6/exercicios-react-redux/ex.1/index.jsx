f9cc1e0e0d8b3519ea0a4bccd8620fa7b64dad49
jhonatankolen <jhonatankolen@b532a151-d5e8-4e7d-bcad-1a85d0c4e1cd>
2017-06-22 02:04:12 +0000

    Seção 6 - React com Redux - 67 - Ex 02 - Integrando o React-Redux (Parte 1)
    - Criando as actions creators e os reducers

---
+++ trunk/udemy-cursos/react-redux/secao6/exercicios-react-redux/ex.1/index.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Field from './field'
+
+//createStore - cria o estado unico, o estado pode ter varios atributos, varios estados da aplicacao
+//combineReducers - liga os reducers para cada atributo criado na store, gerando um objeto unico com os estados
+import { combineReducers, createStore } from 'redux'
+//Componente que liga os estados criados com o redux para os componentes do react
+import { Provider } from 'react-redux'
+
+//reducer do field
+import fieldReducer from './fieldReducer'
+
+//ligando os reducers com os campos
+//um campo sempre eh ligado a um reducer
+//reducer - uma funcao
+const reducers = combineReducers({
+    //utilizando o reducer do field
+    field: fieldReducer
+})
+
+ReactDOM.render(
+    //criando provide fornecendo a store com os reducers
+    <Provider store={createStore(reducers)}>
+        <Field initialValue='Testai' />
+    </Provider>,
+    document.getElementById('app'))
