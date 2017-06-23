1b640cea9f45ea7bd5eb654fd18411d288faa9c7
jhonatankolen <jhonatankolen@b532a151-d5e8-4e7d-bcad-1a85d0c4e1cd>
2017-06-23 20:08:54 +0000

    Seção 7 - TodoApp (Migração para Redux) - 70 - Configurando os reducers
    - Adicionando arquivo que conterá as definições dos reducers

---
+++ react-redux/secao7/todo-app/frontend/src/main/reducers.js
@@ -0,0 +1,25 @@
+import { combineReducers } from 'redux'
+
+//reducer que enblogabara todos os reducer da aplicao
+const rootReducer = combineReducers({
+	todo: () => ({
+		description: 'Ler Livro',
+		list: [{
+			_id: 1,
+			description: 'Pagar fatura do cartão',
+			done: true
+		},
+		{
+			_id: 3,
+			description: 'Jogar games irados',
+			done: false
+		},
+		{
+			_id: 3,
+			description: 'Consulta na terça',
+			done: false
+		},]
+	})
+})
+
+export default rootReducer
