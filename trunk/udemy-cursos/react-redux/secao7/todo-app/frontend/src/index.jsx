afd9c05b169f902d954b8bacbbffed2dfc3782f8
jhonatankolen <jhonatankolen@b532a151-d5e8-4e7d-bcad-1a85d0c4e1cd>
2017-06-24 05:11:57 +0000

    Seção 7 - TodoApp (Migração para Redux) - 75 - Configurando o Redux Dev Tools
    - Configurando o plugin do redux em sincronismo com a aplicacao
    - const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()

--- trunk/udemy-cursos/react-redux/secao7/todo-app/frontend/src/index.jsx
+++ trunk/udemy-cursos/react-redux/secao7/todo-app/frontend/src/index.jsx
@@ -7,8 +7,12 @@ import App from './main/app'
 //Reducers da aplicacao
 import reducers from './main/reducers'
 
+//Configurando o plugin do redux em sincronismo com a aplicacao
+const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ 
+    && window.__REDUX_DEVTOOLS_EXTENSION__()
+
 //criacao do store
-const store = createStore(reducers)
+const store = createStore(reducers, devTools)
 
 ReactDOM.render(
     //aplicando o connector do redux para os componentes do react
