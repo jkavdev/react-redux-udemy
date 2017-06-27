a45f006b519e72122df5038ac0d98a7c32e8f59a
jhonatankolen <jhonatankolen@b532a151-d5e8-4e7d-bcad-1a85d0c4e1cd>
2017-06-27 18:19:11 +0000

    Seção 7 - TodoApp (Migração para Redux) - 76 - Action Creator de Pesquisa
    - Criando ação de pesquisa, mas deparando com problema de sincronicidade de resposta do método

--- trunk/udemy-cursos/react-redux/secao7/todo-app/frontend/src/todo/todoForm.jsx
+++ trunk/udemy-cursos/react-redux/secao7/todo-app/frontend/src/todo/todoForm.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { connect } from 'react-redux'
 //Ligara as actions as reducers
 import { bindActionCreators } from 'redux'
@@ -7,39 +7,52 @@ import Grid from '../template/grid'
 import IconButton from '../template/iconButton'
 
 //As actions do componente
-import { changeDescription } from './todoActions'
+import { changeDescription, search } from './todoActions'
 
-const TodoForm = props => {
+class TodoForm extends Component {
+  constructor(props) {
+    super(props)
 
-    const keyHandler = (e) => {
-        if (e.key === 'Enter') {
-            e.shiftKey ? props.handleSearch() : props.handleAdd()
-        } else if (e.key === 'Escape') {
-            props.handleClear()
-        }
+    this.keyHandler = this.keyHandler.bind()
+  }
+
+  //Utilizando o ciclo de vida do react
+  //este metodo sera chamado antes do componente ser renderizado
+  componenteWillMount() {
+    this.props.search()
+  }
+
+  keyHandler(e) {
+    if (e.key === 'Enter') {
+      e.shiftKey ? this.props.handleSearch() : this.props.handleAdd()
+    } else if (e.key === 'Escape') {
+      this.props.handleClear()
     }
+  }
 
+  render() {
     return (
-        <div role='form' className='todoForm'>
-            <Grid cols='12 9 10'>
-                <input id='description' className='form-control'
-                    placeholder='Adicione uma tarefa'
-                    onChange={props.changeDescription}
-                    onKeyUp={keyHandler}
-                    value={props.description} />
-            </Grid>
+      <div role='form' className='todoForm'>
+        <Grid cols='12 9 10'>
+          <input id='description' className='form-control'
+            placeholder='Adicione uma tarefa'
+            onChange={this.props.changeDescription}
+            onKeyUp={this.keyHandler}
+            value={this.props.description} />
+        </Grid>
 
-            <Grid cols='12 3 2'>
-                <IconButton style='primary' icon='plus' onClick={props.handleAdd} alt='Enter'></IconButton>
-                <IconButton style='info' icon='search' onClick={props.handleSearch} alt='Enter + Shift'></IconButton>
-                <IconButton style='clear' icon='close' onClick={props.handleClear} alt='Esc'></IconButton>
-            </Grid>
-        </div>
+        <Grid cols='12 3 2'>
+          <IconButton style='primary' icon='plus' onClick={this.props.handleAdd} alt='Enter'></IconButton>
+          <IconButton style='info' icon='search' onClick={this.props.handleSearch} alt='Enter + Shift'></IconButton>
+          <IconButton style='clear' icon='close' onClick={this.props.handleClear} alt='Esc'></IconButton>
+        </Grid>
+      </div>
     )
+  }
 }
 
 const mapStateToProps = state => ({ description: state.todo.description })
 //Atraves do dispatch que realizara as chamadas para a ligacao dos reducers
-const mapDispatchToPros = dispath => bindActionCreators({ changeDescription }, dispath)
+const mapDispatchToPros = dispath => bindActionCreators({ changeDescription, search }, dispath)
 
 export default connect(mapStateToProps, mapDispatchToPros)(TodoForm)
