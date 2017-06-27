5a5682712a1db92cca3b1401f917569864e8ab81
jhonatankolen <jhonatankolen@b532a151-d5e8-4e7d-bcad-1a85d0c4e1cd>
2017-06-27 18:19:11 +0000

    Seção 7 - TodoApp (Migração para Redux) - 76 - Action Creator de Pesquisa
    - Criando ação de pesquisa, mas deparando com problema de sincronicidade de resposta do método

--- react-redux/secao7/todo-app/frontend/src/todo/todoActions.js
+++ react-redux/secao7/todo-app/frontend/src/todo/todoActions.js
@@ -1,5 +1,20 @@
+import axios from 'axios'
+
+const URL = 'http://localhost:3003/api/todos'
+
 //Acao que recebe um evento e retorna um objeto com tipo e valor
 export const changeDescription = event => ({
     type: 'DESCRIPTION_CHANGED',
     payload: event.target.value
 })
+
+//Metodo sincrono, chamado e excutado ao mesmo tempo
+//Como estamos utilizado o axios, estamos realizando uma chamada assincrona
+//Por isso nao da tempo de buscar os resultados e atribuir ao request
+export const search = () => {
+    const request = axios.get(`${URL}?sort-=createdAt`)
+    return {
+        type: 'TODO_SEARCHED',
+        payload: request
+    }
+}
