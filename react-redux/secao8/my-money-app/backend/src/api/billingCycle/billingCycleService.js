5f0abca8c41c72e1f33dc5adafabb1c39c67efca
jhonatankolen <jhonatankolen@b532a151-d5e8-4e7d-bcad-1a85d0c4e1cd>
2017-06-30 16:21:53 +0000

    Seção 8 - Aplicação Final - Ciclos de Pagamentos (Backend) - 94 - Obtendo o Sumário de Pagamentos
    - Criando serviço de sumário de retorna o valor somado de todos os débitos e créditos

--- react-redux/secao8/my-money-app/backend/src/api/billingCycle/billingCycleService.js
+++ react-redux/secao8/my-money-app/backend/src/api/billingCycle/billingCycleService.js
@@ -24,4 +24,20 @@ BillingCycle.route('count', (req, resp, next) => {
   })
 })
 
+BillingCycle.route('summary', (req, resp, next) => {
+  BillingCycle.aggregate({
+    $project: { credit: { $sum: "$credits.value" }, debt: { $sum: "$debts.value" } }
+  }, {
+      $group: { _id: null, credit: { $sum: "$credit" }, debt: { $sum: "$debt" } }
+    }, {
+      $project: { _id: 0, credit: 1, debt: 1 }
+    }, (error, result) => {
+      if (error) {
+        resp.statue(500).json({ errors: [error] })
+      } else {
+        resp.json(result[0] || { credit: 0, debt: 0 })
+      }
+    })
+})
+
 module.exports = BillingCycle
