b7d1283721a712aeafe02fbb284d3e772782dc56
jhonatankolen <jhonatankolen@b532a151-d5e8-4e7d-bcad-1a85d0c4e1cd>
2017-06-30 21:20:06 +0000

    Seção 8 - Aplicação Final - Ciclos de Pagamentos (Backend) - 97 - Middleware Query Parser Int
    - Habilitando a conversão dos parâmetros da requisição para inteiros com Query Parser Int
    - Para realizar uma busca paginada com o mongo
    - http://localhost:3003/api/billingCycles?skip=0&limit=1

--- react-redux/secao8/my-money-app/backend/src/config/server.js
+++ react-redux/secao8/my-money-app/backend/src/config/server.js
@@ -13,10 +13,16 @@ const server = express()
 //definição de acessos a nossa api
 const allowCors = require('./cors')
 
+//Habilitara a conversão dos parametros de string para inteiro
+//recurso necessario para realizar a busca paginada oferecida pelo mongo, mas o mongo aceita os estes valores 
+//como inteiros e uma url eh uma string
+const queryParser = require('express-query-int')
+
 //aplicando os midlewares para cada requisicao realizado
 server.use(bodyParser.urlencoded({ extended: true }))
 server.use(bodyParser.json())
 server.use(allowCors)
+server.use(queryParser())
 
 //atribuindo a porta ao servido e apresentando mensagem de sucesso
 server.listen(port, function () {
