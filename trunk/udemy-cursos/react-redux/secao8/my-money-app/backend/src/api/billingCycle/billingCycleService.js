d399f6e056548e413937ebb641ec5a56058aba1d
jhonatankolen <jhonatankolen@b532a151-d5e8-4e7d-bcad-1a85d0c4e1cd>
2017-06-30 17:29:14 +0000

    Seção 8 - Aplicação Final - Ciclos de Pagamentos (Backend) - 95 - Tratando as Mensagens de Erro
    - Criando tratador de erro e aplicando-o ao métodos de post e put

--- trunk/udemy-cursos/react-redux/secao8/my-money-app/backend/src/api/billingCycle/billingCycleService.js
+++ trunk/udemy-cursos/react-redux/secao8/my-money-app/backend/src/api/billingCycle/billingCycleService.js
@@ -1,4 +1,5 @@
 const BillingCycle = require('./billingCycle')
+const errorHandler = require('../common/errorHandler')
 
 //criacao da api rest, com os metodos aceitos
 BillingCycle.methods(['get', 'post', 'put', 'delete'])
@@ -6,6 +7,8 @@ BillingCycle.methods(['get', 'post', 'put', 'delete'])
 //sempre ira retornar um novo registro ao se atualizar, e nao o registro antigo
 //habilitando as validacoes na atualizacao
 BillingCycle.updateOptions({ new: true, runValidators: true })
+//Aplicando middleware de tratador de erro, apos a cada metodo de put e post
+BillingCycle.after('post', errorHandler).after('put', errorHandler)
 
 //Criando rota para o servico de count
 //tera o nome count, e recebe um middleware
@@ -24,17 +27,28 @@ BillingCycle.route('count', (req, resp, next) => {
   })
 })
 
+//Serviço que retorna o sumário de todos os ciclos de pagamento
 BillingCycle.route('summary', (req, resp, next) => {
+  //agrega varias opcoes
   BillingCycle.aggregate({
+    //com o project, indicamos que queremos projetar uma soma dos valores de creditos e debitos
+    //e retornar uma variavel com cada valor obtido de credito e debito
     $project: { credit: { $sum: "$credits.value" }, debt: { $sum: "$debts.value" } }
   }, {
+      //com o group estaremos agrupando todos os registros obtidos pelo project, e somando seus valores
+      //ele tem que ter especificado _id, mas podemos indicar como null
       $group: { _id: null, credit: { $sum: "$credit" }, debt: { $sum: "$debt" } }
     }, {
+      //indicando que sera retornado o credito, e debito, _id nao
       $project: { _id: 0, credit: 1, debt: 1 }
+      //feito isso
     }, (error, result) => {
+      //se houver erro adiciona na lista
       if (error) {
         resp.statue(500).json({ errors: [error] })
       } else {
+        //senao, envia o resultado, como o resultado e um array indicamos a primeira posicao
+        //se nao houver o sumario, nos retorna um objeto com credito e debito zerado
         resp.json(result[0] || { credit: 0, debt: 0 })
       }
     })
