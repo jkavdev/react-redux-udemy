3c2eb816bbe906942e90efc81d43f463c71d92a8
jhonatankolen <jhonatankolen@b532a151-d5e8-4e7d-bcad-1a85d0c4e1cd>
2017-06-30 02:27:39 +0000

    Seção 8 - Aplicação Final - Ciclos de Pagamentos (Backend) - 93 - Obtendo a Qtde de Registros (Count)
    - Criação da rota para o serviço de contagem de registros, e definição dos erros retornados em um array

--- react-redux/secao8/my-money-app/backend/src/api/billingCycle/billingCycleService.js
+++ react-redux/secao8/my-money-app/backend/src/api/billingCycle/billingCycleService.js
@@ -7,4 +7,21 @@ BillingCycle.methods(['get', 'post', 'put', 'delete'])
 //habilitando as validacoes na atualizacao
 BillingCycle.updateOptions({ new: true, runValidators: true })
 
+//Criando rota para o servico de count
+//tera o nome count, e recebe um middleware
+BillingCycle.route('count', (req, resp, next) => {
+  //o mongo ja nos fornece este servico de contagem
+  // em que recebe um callback contendo um erro e o valor retornado
+  BillingCycle.count((error, value) => {
+    //se aconteceu algum erro
+    if (error) {
+      //sera armazenado em um array
+      resp.statue(500).json({ errors: [error] })
+    } else {
+      //senao retorna o valor, ambos erro ou value serao retornados em um objeto
+      resp.json({ value })
+    }
+  })
+})
+
 module.exports = BillingCycle
