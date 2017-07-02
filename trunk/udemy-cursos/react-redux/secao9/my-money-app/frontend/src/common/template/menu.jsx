4896b002fb7b0119a63f6ce253424c6dc98e0ffe
jhonatankolen <jhonatankolen@b532a151-d5e8-4e7d-bcad-1a85d0c4e1cd>
2017-07-02 04:31:12 +0000

    Seção 9 - Aplicação Final - Ciclos de Pagamentos (Frontend) - 106 - Componente Menu (Parte 2)
    - Criando componentes menuTree.jsx agrupador de menuItem.jsx

--- trunk/udemy-cursos/react-redux/secao9/my-money-app/frontend/src/common/template/menu.jsx
+++ trunk/udemy-cursos/react-redux/secao9/my-money-app/frontend/src/common/template/menu.jsx
@@ -1,6 +1,14 @@
 import React from 'react'
 
+import MenuItem from './menuItem'
+import MenuTree from './menuTree'
+
+//Podemos usar menuItem para item no menu ou menuTree para um agrupador de itens de menu
 export default props => (
   <ul className='sidebar-menu'>
+    <MenuItem path='#' label='Dashboard' icon='dashboard' />
+    <MenuTree label='Cadastro' icon='edit'>
+      <MenuItem path='#billingCycles' label='Ciclos de Pagamentos' icon='usd' />
+    </MenuTree>
   </ul>
 )
